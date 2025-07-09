7eba07306a2f6d87d2ef1d524edd87e991aec27a
me21jarus <134506803+me21jarus@users.noreply.github.com>
2025-07-09 12:09:02 +0530

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  final String apiKey = 'your_openweather_api_key'; // Replace with your key
+  final String apiKey = '6336c8f04bbe2bf18819facb79449992'; // Replace with your key
 
   Future<Map<String, dynamic>> getWeather(double lat, double lon) async {
     final url = Uri.parse(
